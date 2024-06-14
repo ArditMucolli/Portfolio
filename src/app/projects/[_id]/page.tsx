8d995cbcdaa89e8e5875bdee3c373e21ab2ddf1c
ArditMucolli <mucolliardit@gmail.com>
2024-06-15 01:48:49 +0200

    feat: add motion animation on project page

--- src/app/projects/[_id]/page.tsx
+++ src/app/projects/[_id]/page.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import { getProject, getSocials } from "@/utils/sanity-utis";
 import { ProjectType } from "@/app/types/ProjectsType";
 import { Skills } from "@/app/types/SkillType";
+import Motion from "@/components/motion/Motion";
 
 type P = {
   params: { _id: string };
@@ -54,62 +55,64 @@ const ProjectPage = ({ params }: P) => {
   return (
     <section className="w-full bg-[#F1F6F9]">
       <Navbar socialsData={socialData} />
-      <div className="min-h-screen flex flex-col max-w-7xl mx-auto my-4 md:my-8 p-5 space-y-4">
-        <h1 className="font-extrabold text-3xl">{projectData.title}</h1>
-        <div className="flex items-center justify-between">
-          <div className="flex items-center space-x-2 text-sm text-violet-500">
-            <p>Created at:</p>
-            <p className="font-bold">
-              {projectData._createdAt.toString().slice(0, 10)}
-            </p>
-          </div>
-          <div className="ml-auto flex mb-5 space-x-4">
-            {projectData.demo && (
-              <button className="text-[#F1F6F9] md:font-bold bg-violet-500 px-2 md:px-4 py-2 rounded-lg hover:bg-violet-300 text-sm">
-                <a
-                  href={projectData.demo}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Demo
-                </a>
-              </button>
-            )}
-            {projectData.code && (
-              <button className="text-[#F1F6F9] md:font-bold bg-violet-500 px-2 md:px-4 py-2 rounded-lg hover:bg-violet-300 text-sm">
-                <a
-                  href={projectData.code}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Code
-                </a>
-              </button>
-            )}
-          </div>
-        </div>
-        <div className="w-full md:max-w-200 flex items-center justify-center">
-          <Image
-            src={projectData.image}
-            alt={projectData.title}
-            className="w-full object-cover border-2 border-violet-500"
-            width={500}
-            height={500}
-            style={{ maxWidth: "800px", textAlign: "center" }}
-          />
-        </div>
-        <p>{projectData.description}</p>
-        <div className="flex items-center space-x-4 text-violet-500">
-          <h3>Tech used:</h3>
-          <div className="flex items-center space-x-2 text-sm font-bold">
-            {projectData.tech.map((tech: Skills, index: number) => (
-              <p key={index} className="bg-violet-200 rounded-lg p-1">
-                {tech.title}
+      <Motion delay={1} direction="down">
+        <div className="min-h-screen flex flex-col max-w-7xl mx-auto my-4 md:my-8 p-5 space-y-4">
+          <h1 className="font-extrabold text-3xl">{projectData.title}</h1>
+          <div className="flex items-center justify-between">
+            <div className="flex items-center space-x-2 text-sm text-violet-500">
+              <p>Created at:</p>
+              <p className="font-bold">
+                {projectData._createdAt.toString().slice(0, 10)}
               </p>
-            ))}
+            </div>
+            <div className="ml-auto flex mb-5 space-x-4">
+              {projectData.demo && (
+                <button className="text-[#F1F6F9] md:font-bold bg-violet-500 px-2 md:px-4 py-2 rounded-lg hover:bg-violet-300 text-sm">
+                  <a
+                    href={projectData.demo}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Demo
+                  </a>
+                </button>
+              )}
+              {projectData.code && (
+                <button className="text-[#F1F6F9] md:font-bold bg-violet-500 px-2 md:px-4 py-2 rounded-lg hover:bg-violet-300 text-sm">
+                  <a
+                    href={projectData.code}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Code
+                  </a>
+                </button>
+              )}
+            </div>
+          </div>
+          <div className="w-full md:max-w-200 flex items-center justify-center">
+            <Image
+              src={projectData.image}
+              alt={projectData.title}
+              className="w-full object-cover border-2 border-violet-500"
+              width={500}
+              height={500}
+              style={{ maxWidth: "800px", textAlign: "center" }}
+            />
+          </div>
+          <p>{projectData.description}</p>
+          <div className="flex items-center space-x-4 text-violet-500">
+            <h3>Tech used:</h3>
+            <div className="flex items-center space-x-2 text-sm font-bold">
+              {projectData.tech.map((tech: Skills, index: number) => (
+                <p key={index} className="bg-violet-200 rounded-lg p-1">
+                  {tech.title}
+                </p>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
+      </Motion>
       <Footer social={socialData} />
     </section>
   );
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,6 +6,7 @@ import Project from "@/components/projects/Project";
 import Footer from "@/components/footer/Footer";
 import { useData } from "@/context/DataContext";
 import { ProjectType } from "../types/ProjectsType";
+import Motion from "@/components/motion/Motion";
 
 const ProjectsPage = () => {
   const { projects, socials } = useData();
@@ -17,13 +18,15 @@ const ProjectsPage = () => {
   return (
     <section className="w-full bg-[F1F6f9]">
       <Navbar socialsData={socials} />
-      <div className="max-w-7xl min-h-screen mx-auto p-5">
-        <div className="w-full p-5 my-4 md:my-16 grid grid-cols-1 md:grid-cols-3 gap-12">
-          {projects.map((project: ProjectType, index: number) => (
-            <Project key={index} projects={project} />
-          ))}
+      <Motion delay={1} direction="down">
+        <div className="max-w-7xl min-h-screen mx-auto p-5">
+          <div className="w-full p-5 my-4 md:my-16 grid grid-cols-1 md:grid-cols-3 gap-12">
+            {projects.map((project: ProjectType, index: number) => (
+              <Project key={index} projects={project} />
+            ))}
+          </div>
         </div>
-      </div>
+      </Motion>
       <Footer social={socials} />
     </section>
   );
