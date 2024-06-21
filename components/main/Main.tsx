41d72355fdc8969aa9f7b1c658241f7a0baf9f19
ArditMucolli <mucolliardit@gmail.com>
2024-06-22 00:42:49 +0200

    fix: main component

--- components/main/Main.tsx
+++ components/main/Main.tsx
@@ -9,7 +9,7 @@ const Main = () => {
 
   if (loading) {
     return (
-      <div className="flex justify-center items-center h-screen">
+      <div className="flex justify-center items-center h-screen bg-white">
         <RingLoader color="#4F46E5" loading={loading} size={80} />
       </div>
     );
@@ -17,27 +17,41 @@ const Main = () => {
 
   if (!profile) {
     return (
-      <p className="text-center text-xl font-bold">No profile data available</p>
+      <div className="flex justify-center items-center h-screen bg-white">
+        <p className="text-center text-xl font-bold">
+          No profile data available
+        </p>
+      </div>
     );
   }
 
   const { name, profileImage, occupation, location, bio } = profile;
 
   return (
-    <section className="min-h-[calc(100vh-56px)] w-full flex flex-col items-center justify-center bg-white p-8">
-      {profileImage && (
-        <Image
-          src={profileImage}
-          alt={`${name}'s profile image`}
-          className="rounded-md shadow-md"
-          height={200}
-          width={200}
-        />
-      )}
-      <h1 className="text-3xl font-bold text-gray-800 mt-6">{name}</h1>
-      {occupation && <p className="text-lg text-gray-600 mt-2">{occupation}</p>}
-      {location && <p className="text-lg text-gray-600">{location}</p>}
-      <p className="text-center text-gray-700 mt-4 w-3/4 md:w-2/5">{bio}</p>
+    <section className="min-h-screen flex flex-col items-center justify-center bg-white">
+      <div className="max-w-3xl w-full p-8">
+        <div className="flex items-center space-x-2">
+          <div className="flex-1">
+            <h1 className="text-3xl font-bold text-gray-800">{name}</h1>
+            {occupation && (
+              <p className="text-lg text-gray-600 mt-2">{occupation}</p>
+            )}
+            {location && <p className="text-lg text-gray-600">{location}</p>}
+          </div>
+          {profileImage && (
+            <div className="relative rounded-full overflow-hidden h-32 w-32">
+              <Image
+                src={profileImage}
+                alt={`${name}'s profile image`}
+                layout="fill"
+                objectFit="cover"
+                className="rounded-full"
+              />
+            </div>
+          )}
+        </div>
+        <p className="text-center text-gray-700 mt-4">{bio}</p>
+      </div>
     </section>
   );
 };
