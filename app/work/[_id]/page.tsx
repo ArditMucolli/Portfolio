9e9db0df421b07220582ad786c7f9866604180b8
ArditMucolli <mucolliardit@gmail.com>
2024-06-25 19:38:30 +0200

    fix: experience page

--- app/work/[_id]/page.tsx
+++ app/work/[_id]/page.tsx
@@ -3,7 +3,6 @@
 import { useState, useEffect, useMemo } from "react";
 import { RingLoader } from "react-spinners";
 import { useData } from "@/context/DataContext";
-import { WorkType } from "@/types/WorkType";
 
 type P = {
   params: { _id: string };
@@ -11,11 +10,8 @@ type P = {
 
 const WorkPage = ({ params }: P) => {
   const _id = params._id;
-
   const [loading, setLoading] = useState(true);
-
   const data = useData();
-
   const experiences = useMemo(() => data?.experiences ?? [], [data]);
 
   useEffect(() => {
@@ -24,9 +20,9 @@ const WorkPage = ({ params }: P) => {
     }
   }, [experiences]);
 
-  const experiencesData = experiences.find((project) => project._id === _id);
+  const experience = experiences.find((project) => project._id === _id);
 
-  function formatDate(dateString: string) {
+  const formatDate = (dateString: string) => {
     const options: Intl.DateTimeFormatOptions = {
       year: "numeric",
       month: "long",
@@ -34,8 +30,9 @@ const WorkPage = ({ params }: P) => {
     };
     const date = new Date(dateString);
     return date.toLocaleDateString("en-US", options);
-  }
+  };
 
+  // Loading State
   if (loading) {
     return (
       <>
@@ -46,45 +43,51 @@ const WorkPage = ({ params }: P) => {
     );
   }
 
-  if (!experiencesData) {
+  // Error State if no data found
+  if (!experience) {
     return (
-      <>
-        <div className="flex justify-center items-center h-screen">
-          <div>Project not found.</div>
+      <div className="flex flex-col justify-center items-center h-screen  p-4">
+        <div className="text-center text-gray-700">
+          <h1 className="text-3xl font-bold mb-4">Project not found</h1>
+          <p>
+            The project you are looking for might have been removed or does not
+            exist.
+          </p>
         </div>
-      </>
+      </div>
     );
   }
 
   return (
-    <div className="mt-20 flex flex-col min-h-screen">
-      <section className=" max-w-3xl w-full p-8 mx-auto text-center py-16 mt-10">
+    <div className="mt-16 flex flex-col min-h-screen ">
+      <section className="max-w-3xl w-full p-8 mx-auto text-center py-16 mt-10">
         <div className="">
           <h2 className="text-3xl font-semibold mb-4 text-black">
-            {experiencesData.title}
+            {experience.title}
           </h2>
-          <p className="text-gray-600 font-semibold">
-            {experiencesData.company}
-          </p>
+          <p className="text-gray-600 font-semibold">{experience.company}</p>
           <p className="text-gray-600">
-            {formatDate(experiencesData.startDate)} -{" "}
-            {experiencesData.endDate
-              ? formatDate(experiencesData.endDate)
-              : "Present"}
+            {formatDate(experience.startDate)} -{" "}
+            {experience.endDate ? formatDate(experience.endDate) : "Present"}
           </p>
-          <p className="text-gray-800 mt-4 leading-6">
-            {experiencesData.description}
-          </p>
-          {experiencesData.tasks && experiencesData.tasks.length > 0 && (
-            <div className="mt-6">
-              <h3 className="text-lg font-semibold mb-2 text-black">
+          <div className="mt-6 text-left">
+            <p className="text-lg text-gray-800 leading-7  p-4">
+              {experience.description}
+            </p>
+          </div>
+          {experience.tasks && experience.tasks.length > 0 && (
+            <div className="mt-10 text-left">
+              <h3 className="text-xl font-semibold mb-2 text-gray-900">
                 Responsibilities Spotlight
               </h3>
-              {experiencesData.tasks.map((task, taskIndex) => (
-                <div key={taskIndex} className="mt-2">
-                  <p className="text-sm text-gray-800">{task.description}</p>
-                </div>
-              ))}
+              <ul className="list-disc list-inside space-y-2 text-gray-800 p-4">
+                {experience.tasks.map((task, index) => (
+                  <li key={index} className="text-lg flex items-start">
+                    <span className="mr-2 mt-1 text-green-500">*</span>{" "}
+                    {task.description}
+                  </li>
+                ))}
+              </ul>
             </div>
           )}
         </div>
