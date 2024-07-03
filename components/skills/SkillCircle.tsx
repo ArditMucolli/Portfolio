58d60a6e9d7f42e74b97e7b4327eab6c26c22c50
ArditMucolli <mucolliardit@gmail.com>
2024-07-03 20:32:43 +0200

    fix: changes on skillCircle

--- components/skills/SkillCircle.tsx
+++ components/skills/SkillCircle.tsx
@@ -8,7 +8,7 @@ type P = {
 const SkillCircle = ({ skills }: P) => {
   return (
     <div className="h-24 w-24 flex justify-center items-center  duration-300 hover:scale-110">
-      <div className="flex justify-center items-center bg-[#F1F6F9] rounded-full h-full min-w-full">
+      <div className="flex justify-center items-center  h-full min-w-full">
         <Image src={skills.image} alt={skills.title} width={150} height={150} />
       </div>
     </div>
